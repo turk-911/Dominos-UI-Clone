626a47d4b6e3cdbbc3e0f4e25f57cf028a155572
Mokshe Jain <147153672+turk-911@users.noreply.github.com>
2024-08-08 03:32:43 +0530

    First Commit

---
+++ data/pizzaMania.js
@@ -0,0 +1,32 @@
+export default [
+    {
+        id: '0',
+        name: 'Veg Loaded',
+        description: 'Tomato | Grilled Mushroom | Jalapeno | Golden Corn | Beans in a fresh pan crust',
+        image: 'https://www.dominos.co.in/files/item/PrimeLoadedL.jpg',
+        price: 100,
+        veg: true,
+    },
+    {
+        id: '1',
+        name: 'Paneer and Onion',
+        description: 'Creamy Paneer and Onion',
+        image: 'https://dominos.co.in/files/items/PrimeCheesyL.jpg',
+        price: 120,
+        veg: true,
+    },
+    {
+        id: '3',
+        name: 'Onion Veg',
+        description: 'A hugely popular margherita, with a deliciously tangy single cheese topping',
+        image: 'https://www.dominos.co.in/files/items/onion_veg.jpg',
+        price: 130,
+        veg: true,
+    },
+    {
+        id: '4',
+        name: 'Golden Corn',
+        description: 'Golden Sweet Corn Pizza',
+        image: 'https://www.dominos.co.in/files/item/golden_corn_veg.jpg',
+    }
+];
---
+++ data/sides.js
@@ -0,0 +1,33 @@
+export default [
+    {
+        id: '0',
+        name: 'Chicken Parcel',
+        description: 'Filled with zingy and tender chicken inside, wrapped in a parcel',
+        image: 'https://www.dominos.co.in/files/items/150135_Aha_Non_Veg_439x307-01.jpg',
+        price: 80,
+        quantity: 1,
+    },
+    {
+        id: '1',
+        name: 'Taco Chicken',
+        description: 'A crispy flaky wrap filled with Mexican Arancini',
+        image: 'https://www.dominos.co.in/files/items/Main_Menu-NVG.jpg',
+        price: 120,
+        quantity: 1,
+    },
+    {
+        id: '2',
+        name: '7 Up (500ml)',
+        description: 'A cool drink to compliment your orders',
+        image: 'https://www.dominos.co.in/files/items/7up.png',
+        price: 56,
+        quantity: 1,
+    },
+    {
+        id: '3',
+        name: 'Choco Lava Cake',
+        description: 'Volcano cake filled with molten choco lava cake inside',
+        price: 80,
+        quantity: 1,
+    },
+];
