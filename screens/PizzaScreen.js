8961489bedaba3e537187f23bcd6b5a9a0559ddb
Mokshe Jain <147153672+turk-911@users.noreply.github.com>
2024-08-10 10:56:52 +0530

    Added Pizza Screen

---
+++ screens/PizzaScreen.js
@@ -0,0 +1,58 @@
+import { useNavigation } from "@react-navigation/native";
+import pizza from "../data/pizza";
+import { useContext } from "react";
+import { CartItems } from "../Context";
+import { FlatList, Pressable, SafeAreaView, Text } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import PizzaComponent from "../components/PizzaComponent";
+
+export default function PizzaScreen() {
+  const data = pizza;
+  const navigation = useNavigation();
+  const { cart, setCart } = useContext(CartItems);
+  const total = cart
+    .map((item) => Number(item.price * item.quantity))
+    .reduce((prev, curr) => prev + curr, 0);
+  console.log(cart, "cart items added");
+  console.log(total, "total price");
+  return (
+    <SafeAreaView>
+      <Ionicons
+        onPress={() => navigation.goBack()}
+        name="arrow-black-circle"
+        size={24}
+        color="black"
+      />
+      <FlatList
+        numColumns={2}
+        showsVerticalScrollIndicator={false}
+        data={data}
+        renderItem={({ item }) => <PizzaComponent pizza={item} />}
+      />
+      {total === 0 ? null : (
+        <Pressable
+          onPress={() => navigation.navigate("Cart")}
+          style={{
+            backgroundColor: "green",
+            padding: 10,
+            position: "absolute",
+            bottom: 100,
+            left: 150,
+            borderRadius: 6,
+          }}
+        >
+          <Text
+            style={{
+              textAlign: "center",
+              fontWeight: "bold",
+              fontSize: 16,
+              color: "white",
+            }}
+          >
+            Go to Cart
+          </Text>
+        </Pressable>
+      )}
+    </SafeAreaView>
+  );
+}
